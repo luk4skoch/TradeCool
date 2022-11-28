7e8201e49b86bb3c1ab1e977b9481e576fe72bc1
luk4skoch <luk4s.koch@gmail.com>
2022-11-28 14:05:51 +0100

    added backend - java - spring

---
+++ backend/tauschcool/src/main/java/com/codecool/tauschcool/TauschcoolApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.tauschcool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TauschcoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TauschcoolApplication.class, args);
+	}
+
+}
