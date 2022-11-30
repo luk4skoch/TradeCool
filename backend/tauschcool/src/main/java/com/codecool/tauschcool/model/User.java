fd1ebacfb980953a0c149cb67b3bdbef0952b121
luk4skoch <luk4s.koch@gmail.com>
2022-11-30 11:04:18 +0100

    created data class 'User'

---
+++ backend/tauschcool/src/main/java/com/codecool/tauschcool/model/User.java
@@ -0,0 +1,76 @@
+package com.codecool.tauschcool.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class User {
+
+    private int id;
+
+    private String name;
+
+    private String email;
+
+    private String location;
+
+    private List<Product> productList;
+
+    public User(int id, String name, String email, String location) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.location = location;
+        productList = new ArrayList<>();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public List<Product> getProductList() {
+        return productList;
+    }
+
+    public void addProduct(List<Product> productList) {
+        this.productList = productList;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", location='" + location + '\'' +
+                ", productList=" + productList +
+                '}';
+    }
+}
