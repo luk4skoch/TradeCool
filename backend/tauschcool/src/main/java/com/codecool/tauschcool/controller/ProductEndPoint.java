07ec5e8ddf321f2af73f6a0919aaa9fcb299e083
Weronika <weronika.lopata@gmail.com>
2022-12-12 15:04:00 +0100

    refactored Service, added Repository for products, resolved some tipps from El

---
+++ backend/tauschcool/src/main/java/com/codecool/tauschcool/controller/ProductEndPoint.java
@@ -0,0 +1,53 @@
+package com.codecool.tauschcool.controller;
+
+import com.codecool.tauschcool.model.Product;
+import com.codecool.tauschcool.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
+@RequestMapping("api/products")
+public class ProductEndPoint {
+
+    private final ProductService productService;
+
+    @Autowired
+    public ProductEndPoint(ProductService service) {
+        this.productService = service;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Product>> getProductList() {
+        return new ResponseEntity<>(productService.getProductList(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable Long id) {
+        Optional<Product> product = productService.getProductById(id);
+        return product
+                .map(value -> new ResponseEntity<>(value, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(null, HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping
+    public ResponseEntity<Product> addNewProduct(@RequestBody Product product) {
+        return new ResponseEntity<>(productService.addNewProduct(product), HttpStatus.CREATED);
+    }
+
+    @PutMapping
+    public ResponseEntity<Product> editProductById(@RequestBody Product product) {
+        return new ResponseEntity<>(productService.editProductById(product), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteProductById(@PathVariable Long id) {
+        productService.deleteProductById(id);
+
+    }
+}
--- backend/tauschcool/src/main/java/com/codecool/tauschcool/model/Product.java
+++ backend/tauschcool/src/main/java/com/codecool/tauschcool/model/Product.java
@@ -1,114 +1,31 @@
 package com.codecool.tauschcool.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import org.json.JSONObject;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Product {
 
 
     @Id
-    private int id;
-
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String title;
-
     private String description;
-
     private String imagePath;
-
     private String category;
-
-    private int userId;
+//    private User user;
 
     private ProductStatus status;
-
-    public Product(int id, String title, String description, String imagePath, String category, int userId, ProductStatus status) {
-        this.id = id;
-        this.title = title;
-        this.description = description;
-        this.imagePath = imagePath;
-        this.category = category;
-        this.userId = userId;
-        this.status = status;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getImagePath() {
-        return imagePath;
-    }
-
-    public void setImagePath(String imagePath) {
-        this.imagePath = imagePath;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    public int getUserId() {
-        return userId;
-    }
-
-    public void setUserId(int userId) {
-        this.userId = userId;
-    }
-
-    public ProductStatus getStatus() {
-        return status;
-    }
-
-    public void setStatus(ProductStatus status) {
-        this.status = status;
-    }
-
-    @Override
-    public String toString() {
-        return "Product{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", description='" + description + '\'' +
-                ", imagePath='" + imagePath + '\'' +
-                ", category='" + category + '\'' +
-                ", userId=" + userId +
-                ", status=" + status +
-                '}';
-    }
-
-    public JSONObject getJson() {
-        JSONObject json = new JSONObject();
-        json.append("title", title);
-        json.append("description", description);
-        json.append("imagePath", imagePath);
-        json.append("category", category);
-        json.append("userId", userId);
-        json.append("status", status.name());
-        return json;
-    }
 }
