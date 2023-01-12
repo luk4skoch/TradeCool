baeac45f155cf3ed16a0f53e6e6565c4a78a0cf4
Weronika <weronika.lopata@gmail.com>
2023-01-12 15:16:23 +0100

    merge and bug fix

--- frontend/tradecool/src/components/MainLayout.js
+++ frontend/tradecool/src/components/MainLayout.js
@@ -2,11 +2,11 @@ import React from "react";
 import MainNavbar from "./MainNavbar";
 import { Outlet } from "react-router";
 import { Container } from "react-bootstrap";
+import Logo from './img/tradecool-scribble.png'
 
 export default function MainLayout() {
   return (
     <div>
-        <MainNavbar navTitle="Trade Cool" />
         <MainNavbar navTitle=<img width="100px" height="auto" className="img-responsive" src={Logo}  alt="logo" /> />
         <Container>
           <Outlet />
