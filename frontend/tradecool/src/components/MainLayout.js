ac47c4e208d96babb7945cb3b5b85f90ca288702
luk4skoch <luk4s.koch@gmail.com>
2023-01-12 14:33:47 +0100

    added logo to home page and navbar

--- frontend/tradecool/src/components/MainLayout.js
+++ frontend/tradecool/src/components/MainLayout.js
@@ -3,6 +3,7 @@ import MainNavbar from "./MainNavbar";
 import { Outlet } from "react-router";
 import { Container } from "react-bootstrap";
 import { UserTokenContext } from "./context/UserToken";
+import Logo from "./img/tradecool.png"
 
 export default function MainLayout() {
   const [userToken, setUserToken] = React.useState(
@@ -12,7 +13,7 @@ export default function MainLayout() {
   return (
     <div>
       <UserTokenContext.Provider value={[userToken, setUserToken]}>
-        <MainNavbar navTitle="Trade Cool" />
+        <MainNavbar navTitle=<img width="100px" height="auto" className="img-responsive" src={Logo}  alt="logo" /> />
         <Container>
           <Outlet />
         </Container>
--- frontend/tradecool/src/components/pages/Home.js
+++ frontend/tradecool/src/components/pages/Home.js
@@ -1,9 +1,11 @@
 import React from "react";
+import logo from "../img/tradecool.png";
 export default function Home() {
   return (
     <div>
         <br/>
-      <h1>Willkommen zu trade-cool.</h1>
+      <h1>Willkommen bei</h1>
+        <img src={logo} width="90%" alt={"logo"}/>
     </div>
   );
 }
