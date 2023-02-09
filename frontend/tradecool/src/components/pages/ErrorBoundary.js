4319e7f7071b54b78b804fa9e10336f6ac7edbd2
Aleksandar Micakovic <daneijin@gmail.com>
2023-02-09 19:19:33 +0100

    before deployment, duplicate ErrorBoundary removed, endpoint Constant updated and fetch cals updated to use constant

--- frontend/tradecool/src/components/pages/ErrorBoundary.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { useRouteError } from "react-router";
-
-export default function ErrorBoundary() {
-  const error = React.useState(useRouteError());
-
-  return <>{error && <p>{error}</p>}</>;
-}
